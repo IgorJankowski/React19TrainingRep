1bd55f75c2219f6b5ea8250fec2ae62ae6c50645
HudorHasHasHa <jankowskiigor5@gmail.com>
2025-07-23 17:43:28 +0200

    add get score method

--- starter/transitions/src/App.jsx
+++ starter/transitions/src/App.jsx
@@ -1,3 +1,17 @@
+import { useState } from "react";
+import getScore from "./getScore";
+
 export default function App() {
+  const [isPending, setIsPending] = useState(true);
+  const [game, setGame] = useState(1);
+  const [score, setScore] = useState({ home: "-", away: "-" });
+
+  async function getNewScore(game) {
+    setIsPending(true);
+    setGame(game);
+    const newScore = await getScore(game);
+    setScore(newScore);
+    setIsPending(false);
+  }
   return <h1>Transitions</h1>;
 }
