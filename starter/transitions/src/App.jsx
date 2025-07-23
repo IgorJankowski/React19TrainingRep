26d5d4aa3520350b74ceb66181048c8548e48ab2
HudorHasHasHa <jankowskiigor5@gmail.com>
2025-07-23 18:16:31 +0200

    feat: implement game score display and selection functionality

--- starter/transitions/src/App.jsx
+++ starter/transitions/src/App.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import getScore from "./getScore";
+import Score from "./Score";
 
 export default function App() {
   const [isPending, setIsPending] = useState(true);
@@ -13,5 +14,40 @@ export default function App() {
     setScore(newScore);
     setIsPending(false);
   }
-  return <h1>Transitions</h1>;
+
+  useEffect(() => {
+    getNewScore(game);
+  }, [game]);
+
+  return (
+    <div className="app">
+      <h1>Game {game}</h1>
+      <select
+        disabled={isPending}
+        onChange={(e) => getNewScore(e.target.value)}
+      >
+        <option value={1}>Game 1</option>
+        <option value={2}>Game 2</option>
+        <option value={3}>Game 3</option>
+        <option value={4}>Game 4</option>
+        <option value={5}>Game 5</option>
+        <option value={6}>Game 6</option>
+        <option value={7}>Game 7</option>
+      </select>
+      <div className={`loading-container ${isPending ? "loading" : ""}`}>
+        <span className="spinner">lol</span>
+      </div>
+      <div>
+        <Score
+          isPending={isPending}
+          homeImage={score.homeImage}
+          homeName={score.homeName}
+          home={score.home}
+          awayImage={score.awayImage}
+          awayName={score.awayName}
+          away={score.away}
+        ></Score>
+      </div>
+    </div>
+  );
 }
