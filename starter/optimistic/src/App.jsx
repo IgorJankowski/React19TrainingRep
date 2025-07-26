0b22d3a5fe73cfe61b862092a171e8cd7e102040
HudorHasHasHa <jankowskiigor5@gmail.com>
2025-07-26 12:46:30 +0200

    - useOptimistic - useTransition

--- starter/optimistic/src/App.jsx
+++ starter/optimistic/src/App.jsx
@@ -1,3 +1,68 @@
+import { useEffect, useOptimistic, useState, useTransition } from "react";
+
 export default function App() {
-  return <h1>Deep Thoughts</h1>;
+  const [isPending, startTransition] = useTransition();
+  const [thoughts, setThoughts] = useState([]);
+  const [thought, setThought] = useState([]);
+  const [optimisticThoughts, addOptimisticThought] = useOptimistic(
+    thoughts,
+    (oldThoughts, newThought) => [newThought, ...oldThoughts]
+  );
+
+  useEffect(() => {
+    fetch("/thoughts")
+      .then((res) => res.json())
+      .then((data) => setThoughts(data));
+  }, []);
+
+  async function postDeepThought() {
+    startTransition(async () => {
+      addOptimisticThought(`${thought} (Sending...)`);
+      setThought("");
+      const response = await fetch("/thoughts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ thought }),
+      });
+
+      if (!response.ok) {
+        alert("Smth went wrong, try again? heh");
+        return;
+      }
+      const { thoughts: newThoughts } = await response.json();
+
+      setThoughts(newThoughts);
+    });
+  }
+
+  return (
+    <div className="app">
+      <h1>Deep Thoughts</h1>
+      <form
+        action=""
+        onSubmit={(e) => {
+          e.preventDefault();
+          postDeepThought();
+        }}
+      >
+        <label>What's on your mind?</label>
+        <textarea
+          id="thought"
+          name="thought"
+          rows={4}
+          cols={33}
+          value={thought}
+          onChange={(e) => setThought(e.target.value)}
+        ></textarea>
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {optimisticThoughts.map((thought, index) => (
+          <li key={index}>{thought}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
